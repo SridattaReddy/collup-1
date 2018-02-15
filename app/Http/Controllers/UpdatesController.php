4fe3de1f7eea22ba3c2e2ba2b0cade267ebc9d1e
raviteja155 <raviteja155@gmail.com>
2018-02-15 12:09:59 -0500

    Empty updates controller with its modal and migration

---
+++ app/Http/Controllers/UpdatesController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Update;
+use Illuminate\Http\Request;
+
+class UpdatesController extends Controller
+{
+    public function index()
+    {
+      
+    }
+  
+    public function create()
+    {
+      
+    }
+  
+    public function store()
+    {
+      
+    }
+  
+    public function show()
+    {
+      
+    }
+  
+    public function edit()
+    {
+      
+    }
+  
+    public function update()
+    {
+      
+    }
+  
+    public function destroy()
+    {
+    
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,24 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Get all the update of college
+Route::get('updates', 'UpdatesController@index')->name('updates.index');
+
+// Create a new update
+Route::get('updates/create', 'UpdatesController@create')->name('updates.create');
+
+// Store new update
+Route::post('updates', 'UpdatesController@store')->name('updates.store');
+
+// Show an update
+Route::get('updates/{update}', 'UpdatesController@show')->name('updates.show');
+
+// Edit an update
+Route::get('updates/{update}/edit', 'UpdatesController@edit')->name('updates.edit');
+
+// Update an update
+Route::patch('updates/{update}', 'UpdatesController@update')->name('updates.update');
+
+// Delete an upda
+Route::delete('updates/{update}', 'UpdatesController@delete')->name('updates.destroy');
