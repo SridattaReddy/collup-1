322585e9dc02214931f8fe6c6e4cef743f2967e9
raviteja155 <raviteja155@gmail.com>
2018-02-15 11:51:48 -0500

    Authentication with Bootstrap master template

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.console')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col">
+          <h2>
+            Welcome to your dashboard
+          </h2>
+      </div>
+    </div>
+</div>
+@endsection
